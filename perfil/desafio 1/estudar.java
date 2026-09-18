b29bac6859be7be150cd139ab66e54cc94efc23a
João Compart <Joaocompart@gmail.com>
2026-09-18 15:57:22 -0300

    finalizacao do desafio 1

---
+++ perfil/desafio 1/estudar.java
@@ -0,0 +1,6 @@
+public class estudar {
+    public static void main(String[] args) {
+        System.out.println("Estuda java por 1 hora");
+        System.out.println("estudar Ia por 30 minutos");
+    }
+}
--- perfil/desafio 1/soma.java
+++ perfil/desafio 1/soma.java
@@ -1,5 +1,7 @@
-public class Soma{
+public class soma{
+
     public static void main(String[] args) {
         System.out.println(10 + 5);
+
     }
-}
+        }
